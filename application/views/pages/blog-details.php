630b9c3ca256b2bd8834c974bd5a54c6169b9037
RidenTech <163792769+Haseeb04092002@users.noreply.github.com>
2026-04-07 20:02:31 +0500

    Update blog-details.php

--- application/views/pages/blog-details.php
+++ application/views/pages/blog-details.php
@@ -39,14 +39,10 @@
 									</div>
 									<div class="info-bx">
 										<ul class="media-post">
-											<li><a href="#"><i class="fa fa-calendar"></i>May 14 2019</a></li>
-											<li><a href="#"><i class="fa fa-comments-o"></i>10 Comment</a></li>
+											<li><i class="fa fa-calendar"></i><?= date('d M Y', strtotime($blog->added_on)) ?></li>
 										</ul>
-										<h5 class="post-title"><a href="#">Why every startup should adopt Amazon’s Hot Air. Why every startup should adopt Amazon’s Hot Air.</a></h5>
-										<p>It is used every day in all types of businesses; Email newsletters, websites, print and online advertisements, presentations, social media updates, flyers, and brochures; the list goes on and on</p>
-										<p>David Ogilvy, the advertising legend once said that, On average, five times as many people read the headline as read the body copy. When you have written your headline, you have spent eighty cents out of your dollar." As Ogilvy points out, your headline is the first (and sometimes the only) thing that your audience will read.</p>
-										<p>You just need to enter the keyword and select the keyword type to generate a list of 6 title ideas and suggestions. If you’re not satisfied with the results, you can always hit the refresh button to generate a new list of unique titles.</p>
-										<p>Once you’ve gotten all the titles and have chosen the best one, the next thing you need to do is to craft a magnetic content. Great content marketers excel at creating content that their readers crave, but even the best struggle with delivering content to the right person at the right time.</p>
+										<h5 class="post-title"><?= $blog->title ?></h5>
+										<p><?= $blog->short_desc ?></p>
 										<div class="ttr-divider bg-gray"><i class="icon-dot c-square"></i></div>
 										<div class="widget_tag_cloud">
 											<h6>TAGS</h6>
@@ -71,7 +67,7 @@
 												<a href="#">Joomla</a>
 											</div>
 										</div>
-										<div class="ttr-divider bg-gray"><i class="icon-dot c-square"></i></div>
+										<!-- <div class="ttr-divider bg-gray"><i class="icon-dot c-square"></i></div>
 										<h6>SHARE </h6>
 										<ul class="list-inline contact-social-bx">
 											<li><a href="#" class="btn outline radius-xl"><i class="fa fa-facebook"></i></a></li>
@@ -79,109 +75,7 @@
 											<li><a href="#" class="btn outline radius-xl"><i class="fa fa-linkedin"></i></a></li>
 											<li><a href="#" class="btn outline radius-xl"><i class="fa fa-google-plus"></i></a></li>
 										</ul>
-										<div class="ttr-divider bg-gray"><i class="icon-dot c-square"></i></div>
-									</div>
-								</div>
-								<div class="clear" id="comment-list">
-									<div class="comments-area" id="comments">
-										<h2 class="comments-title">8 Comments</h2>
-										<div class="clearfix m-b20">
-											<!-- comment list END -->
-											<ol class="comment-list">
-												<li class="comment">
-													<div class="comment-body">
-														<div class="comment-author vcard"> <img class="avatar photo" src="assets/images/testimonials/pic1.jpg" alt=""> <cite class="fn">John Doe</cite> <span class="says">says:</span> </div>
-														<div class="comment-meta"> <a href="#">December 02, 2019 at 10:45 am</a> </div>
-														<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae neqnsectetur adipiscing elit. Nam viae neqnsectetur adipiscing elit.
-															Nam vitae neque vitae sapien malesuada aliquet. </p>
-														<div class="reply"> <a href="#" class="comment-reply-link">Reply</a> </div>
-													</div>
-													<ol class="children">
-														<li class="comment odd parent">
-															<div class="comment-body">
-																<div class="comment-author vcard"> <img class="avatar photo" src="assets/images/testimonials/pic2.jpg" alt=""> <cite class="fn">John Doe</cite> <span class="says">says:</span> </div>
-																<div class="comment-meta"> <a href="#">December 02, 2019 at 10:45 am</a> </div>
-																<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae neque vitae sapien malesuada aliquet.
-																	In viverra dictum justo in vehicula. Fusce et massa eu ante ornare molestie. Sed vestibulum sem felis,
-																	ac elementum ligula blandit ac.</p>
-																<div class="reply"> <a href="#" class="comment-reply-link">Reply</a> </div>
-															</div>
-															<ol class="children">
-																<li class="comment odd parent">
-																	<div class="comment-body">
-																		<div class="comment-author vcard"> <img class="avatar photo" src="assets/images/testimonials/pic3.jpg" alt=""> <cite class="fn">John Doe</cite> <span class="says">says:</span> </div>
-																		<div class="comment-meta"> <a href="#">December 02, 2019 at 10:45 am</a> </div>
-																		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae neque vitae sapien malesuada aliquet.
-																			In viverra dictum justo in vehicula. Fusce et massa eu ante ornare molestie. Sed vestibulum sem felis,
-																			ac elementum ligula blandit ac.</p>
-																		<div class="reply"> <a href="#" class="comment-reply-link">Reply</a> </div>
-																	</div>
-																</li>
-															</ol>
-															<!-- list END -->
-														</li>
-													</ol>
-													<!-- list END -->
-												</li>
-												<li class="comment">
-													<div class="comment-body">
-														<div class="comment-author vcard"> <img class="avatar photo" src="assets/images/testimonials/pic1.jpg" alt=""> <cite class="fn">John Doe</cite> <span class="says">says:</span> </div>
-														<div class="comment-meta"> <a href="#">December 02, 2019 at 10:45 am</a> </div>
-														<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae neque vitae sapien malesuada aliquet.
-															In viverra dictum justo in vehicula. Fusce et massa eu ante ornare molestie. Sed vestibulum sem felis,
-															ac elementum ligula blandit ac.</p>
-														<div class="reply"> <a href="#" class="comment-reply-link">Reply</a> </div>
-													</div>
-												</li>
-												<li class="comment">
-													<div class="comment-body">
-														<div class="comment-author vcard"> <img class="avatar photo" src="assets/images/testimonials/pic2.jpg" alt=""> <cite class="fn">John Doe</cite> <span class="says">says:</span> </div>
-														<div class="comment-meta"> <a href="#">December 02, 2019 at 10:45 am</a> </div>
-														<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae neque vitae sapien malesuada aliquet.
-															In viverra dictum justo in vehicula. Fusce et massa eu ante ornare molestie. Sed vestibulum sem felis,
-															ac elementum ligula blandit ac.</p>
-														<div class="reply"> <a href="#" class="comment-reply-link">Reply</a> </div>
-													</div>
-												</li>
-												<li class="comment">
-													<div class="comment-body">
-														<div class="comment-author vcard"> <img class="avatar photo" src="assets/images/testimonials/pic3.jpg" alt=""> <cite class="fn">John Doe</cite> <span class="says">says:</span> </div>
-														<div class="comment-meta"> <a href="#">December 02, 2019 at 10:45 am</a> </div>
-														<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae neque vitae sapien malesuada aliquet.
-															In viverra dictum justo in vehicula. Fusce et massa eu ante ornare molestie. Sed vestibulum sem felis,
-															ac elementum ligula blandit ac.</p>
-														<div class="reply"> <a href="#" class="comment-reply-link">Reply</a> </div>
-													</div>
-												</li>
-											</ol>
-											<!-- comment list END -->
-											<!-- Form -->
-											<div class="comment-respond" id="respond">
-												<h4 class="comment-reply-title" id="reply-title">Leave a Reply <small> <a style="display:none;" href="#" id="cancel-comment-reply-link" rel="nofollow">Cancel reply</a> </small> </h4>
-												<form class="comment-form" id="commentform" method="post">
-													<p class="comment-form-author">
-														<label for="author">Name <span class="required">*</span></label>
-														<input type="text" value="" name="Author" placeholder="Author" id="author">
-													</p>
-													<p class="comment-form-email">
-														<label for="email">Email <span class="required">*</span></label>
-														<input type="text" value="" placeholder="Email" name="email" id="email">
-													</p>
-													<p class="comment-form-url">
-														<label for="url">Website</label>
-														<input type="text" value="" placeholder="Website" name="url" id="url">
-													</p>
-													<p class="comment-form-comment">
-														<label for="comment">Comment</label>
-														<textarea rows="8" name="comment" placeholder="Comment" id="comment"></textarea>
-													</p>
-													<p class="form-submit">
-														<input type="submit" value="Submit Comment" class="submit" id="submit" name="submit">
-													</p>
-												</form>
-											</div>
-											<!-- Form -->
-										</div>
+										<div class="ttr-divider bg-gray"><i class="icon-dot c-square"></i></div> -->
 									</div>
 								</div>
 								<!-- blog END -->
